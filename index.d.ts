ab152efaa8d60ff0b30a975880322f79d0bf0de6
syumai <syumai@gmail.com>
2021-08-29 23:56:24 +0900

    changed to use pointer capture

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
-import { ReactNode } from "react";
-import "./index.css";
+import { ReactNode, SetStateAction } from "react";
 /***
  * Resizer Component
  */
@@ -11,23 +10,27 @@ declare type ResizerStyleProps = {
 };
 declare type ResizerProps = ResizerStyleProps & {
     position: string;
-    initialPos: number;
-    setWidth: (width: number) => void;
+    containerWidth: number;
+    setContainerWidth: (callback: SetStateAction<number>) => void;
+    zIndex?: number;
 };
-export declare const Resizer: ({ color, hoveredColor, transitionDuration, width, position, initialPos, setWidth, }: ResizerProps) => JSX.Element;
+export declare const Resizer: ({ color, hoveredColor, transitionDuration, width, position, containerWidth, setContainerWidth, zIndex, }: ResizerProps) => JSX.Element;
 /***
  * Layout components
  */
-declare type ChildrenProps = {
+export declare type SidebarLayoutContainerProps = {
+    className?: string;
     children: ReactNode;
 };
-export declare const Sidebar: ({ children }: ChildrenProps) => JSX.Element;
-export declare const PrimaryView: ({ children }: ChildrenProps) => JSX.Element;
-export declare const SecondaryView: ({ children }: ChildrenProps) => JSX.Element;
-export interface SidebarLayoutProps extends ChildrenProps {
+export declare const Sidebar: ({ className, children, }: SidebarLayoutContainerProps) => JSX.Element;
+export declare const PrimaryView: ({ className, children, }: SidebarLayoutContainerProps) => JSX.Element;
+export declare const SecondaryView: ({ className, children, }: SidebarLayoutContainerProps) => JSX.Element;
+export interface SidebarLayoutProps {
+    className?: string;
+    children: ReactNode;
     defaultSidebarWidth?: number;
     defaultSecondaryViewWidth?: number;
 }
-export declare const SidebarLayout: ({ defaultSidebarWidth, defaultSecondaryViewWidth, children, }: SidebarLayoutProps) => JSX.Element;
+export declare const SidebarLayout: ({ className, children, defaultSidebarWidth, defaultSecondaryViewWidth, }: SidebarLayoutProps) => JSX.Element;
 export {};
 //# sourceMappingURL=index.d.ts.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
-import { ReactNode, useCallback, useRef, useState } from "react";
-import styled from "styled-components";
-import "./index.css";
+import {
+  CSSProperties,
+  PointerEvent,
+  ReactNode,
+  SetStateAction,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 
 /***
  * Resizer Component
@@ -13,28 +19,22 @@ type ResizerStyleProps = {
   width?: number;
 };
 
-type ResizerDynamicStyleProps = {
-  left?: number;
-  right?: number;
-  width: number; // must not be optional as dynamic style
-};
-
 type ResizerProps = ResizerStyleProps & {
   position: string;
-  initialPos: number;
-  setWidth: (width: number) => void;
+  containerWidth: number;
+  setContainerWidth: (callback: SetStateAction<number>) => void;
+  zIndex?: number;
 };
 
-const ResizerDiv = styled.div<ResizerStyleProps & ResizerDynamicStyleProps>`
-  background-color: ${(props) => props.color || "transparent"};
-  width: ${(props) => props.width}px;
-  left: ${(props) => `${props.left}px` || "initial"};
-  right: ${(props) => `${props.right}px` || "initial"};
-  transition: background-color ${(props) => props.transitionDuration || 300}ms;
-  &:hover {
-    background-color: ${(props) => props.hoveredColor || "#1e90ff"};
-  }
-`;
+const resizerStyle: CSSProperties = Object.freeze({
+  position: "absolute",
+  height: "100%",
+  MozUserSelect: "none",
+  WebkitUserSelect: "none",
+  msUserSelect: "none",
+  userSelect: "none",
+  cursor: "col-resize",
+});
 
 export const Resizer = ({
   color,
@@ -42,93 +42,66 @@ export const Resizer = ({
   transitionDuration,
   width = 6,
   position,
-  initialPos,
-  setWidth,
+  containerWidth,
+  setContainerWidth,
+  zIndex = 1000,
 }: ResizerProps): JSX.Element => {
-  const resizerRef = useRef(null);
   const [dragging, setDragging] = useState(false);
-  const [xPos, setXPos] = useState(initialPos);
 
-  const mouseDownHandler = useCallback(() => {
+  const pointerDownHandler = useCallback((e: PointerEvent) => {
     setDragging(true);
+    e.currentTarget.setPointerCapture(e.pointerId);
   }, []);
 
-  const mouseUpHandler = useCallback(() => {
+  const pointerUpHandler = useCallback((e: PointerEvent) => {
     setDragging(false);
+    e.currentTarget.releasePointerCapture(e.pointerId);
   }, []);
 
-  const mouseMoveHandler = useCallback(
-    (e: MouseEvent) => {
+  const pointerMoveHandler = useCallback(
+    (e: PointerEvent) => {
       if (!dragging) {
         return;
       }
-      let nextXPos = 0;
-      setXPos((prev) => {
-        const movementX = e.movementX * (position === "right" ? -1 : 1);
-        nextXPos = prev + movementX;
-        return nextXPos;
-      });
-      setWidth(nextXPos);
+      setContainerWidth(
+        (prev) => prev + e.movementX * (position === "right" ? -1 : 1)
+      );
     },
-    [position, dragging, setWidth]
+    [position, dragging, setContainerWidth]
   );
 
-  const addEventListeners = useCallback(
-    (node) => {
-      node.addEventListener("mousedown", mouseDownHandler);
-      node.addEventListener("mouseup", mouseUpHandler);
-      document.addEventListener("mousemove", mouseMoveHandler);
-    },
-    [mouseDownHandler, mouseUpHandler, mouseMoveHandler]
+  const style: CSSProperties = useMemo(
+    () => ({
+      ...resizerStyle,
+      width: `${width}px`,
+      left: position === "left" ? `${containerWidth - width / 2}px` : "initial",
+      right:
+        position === "right" ? `${containerWidth - width / 2}px` : "initial",
+      transition: `background-color ${transitionDuration || 300}ms`,
+      zIndex: zIndex,
+    }),
+    [width, position, containerWidth, transitionDuration]
   );
 
-  const removeEventListeners = useCallback(
-    (node) => {
-      node.removeEventListener("mousedown", mouseDownHandler);
-      node.removeEventListener("mouseup", mouseUpHandler);
-      document.removeEventListener("mousemove", mouseMoveHandler);
-    },
-    [mouseDownHandler, mouseUpHandler, mouseMoveHandler]
-  );
-
-  const resizerCallbackRef = useCallback(
-    (node) => {
-      // both of old and new node are null or undefined
-      if (!resizerRef.current && !node) {
-        return;
-      }
-      // nothing is changed
-      if (resizerRef.current === node) {
-        return;
-      }
-      // resizer is removed
-      if (resizerRef.current && !node) {
-        removeEventListeners(resizerRef.current);
-        resizerRef.current = null;
-        return;
-      }
-      // resizer is changed
-      if (resizerRef.current && node) {
-        removeEventListeners(resizerRef.current);
-      }
-      // resizer is changed or newly attached
-      addEventListeners(node);
-      resizerRef.current = node;
-    },
-    [addEventListeners, removeEventListeners]
-  );
+  const resizerId = `react-sidebar-layout-resizer-${position}`;
 
   return (
-    <ResizerDiv
-      ref={resizerCallbackRef}
-      className="resizer"
-      color={color}
-      hoveredColor={hoveredColor}
-      transitionDuration={transitionDuration}
-      width={width}
-      left={position === "left" ? xPos - width / 2 : undefined}
-      right={position === "right" ? xPos - width / 2 : undefined}
-    />
+    <>
+      <div
+        id={resizerId}
+        style={style}
+        onPointerDown={pointerDownHandler}
+        onPointerMove={pointerMoveHandler}
+        onPointerUp={pointerUpHandler}
+      />
+      <style>{`
+      #${resizerId} {
+        backgroundColor: ${color || "transparent"};
+      }
+      #${resizerId}:hover { background-color: ${
+        hoveredColor || "#1e90ff"
+      }; }`}</style>
+    </>
   );
 };
 
@@ -136,73 +109,98 @@ export const Resizer = ({
  * Layout components
  */
 
-type ChildrenProps = {
+export type SidebarLayoutContainerProps = {
+  className?: string;
   children: ReactNode;
 };
 
-type ContainerProps = ChildrenProps & {
-  className: string;
-};
+const gridAreaSidebar = "react-sidebar-layout-sidebar";
+const gridAreaPrimaryView = "react-sidebar-layout-primary-view";
+const gridAreaSecondaryView = "react-sidebar-layout-secondary-view";
 
-export const Sidebar = ({ children }: ChildrenProps) => (
-  <Container className="sidebar">{children}</Container>
+const sidebarStyle: CSSProperties = Object.freeze({
+  height: "100%",
+  gridArea: gridAreaSidebar,
+});
+const primaryViewStyle: CSSProperties = Object.freeze({
+  height: "100%",
+  gridArea: gridAreaPrimaryView,
+});
+const secondaryViewStyle: CSSProperties = Object.freeze({
+  height: "100%",
+  gridArea: gridAreaSecondaryView,
+});
+
+export const Sidebar = ({
+  className,
+  children,
+}: SidebarLayoutContainerProps): JSX.Element => (
+  <div className={className} style={sidebarStyle}>
+    {children}
+  </div>
+);
+export const PrimaryView = ({
+  className,
+  children,
+}: SidebarLayoutContainerProps): JSX.Element => (
+  <div className={className} style={primaryViewStyle}>
+    {children}
+  </div>
+);
+export const SecondaryView = ({
+  className,
+  children,
+}: SidebarLayoutContainerProps): JSX.Element => (
+  <div className={className} style={secondaryViewStyle}>
+    {children}
+  </div>
 );
 
-export const PrimaryView = ({ children }: ChildrenProps) => (
-  <Container className="primary-view">{children}</Container>
-);
-
-export const SecondaryView = ({ children }: ChildrenProps) => (
-  <Container className="secondary-view">{children}</Container>
-);
-
-const Container = ({ children, className }: ContainerProps): JSX.Element => (
-  <div className={className}>{children}</div>
-);
-
-export interface SidebarLayoutProps extends ChildrenProps {
+export interface SidebarLayoutProps {
+  className?: string;
+  children: ReactNode;
   defaultSidebarWidth?: number;
   defaultSecondaryViewWidth?: number;
 }
 
-type LayoutDynamicStyleProps = {
-  sidebarWidth: number;
-  secondaryViewWidth: number;
-};
-
-const LayoutDiv = styled.div<LayoutDynamicStyleProps>`
-  grid-template-columns: ${(props) => props.sidebarWidth}px 1fr ${(props) =>
-      props.secondaryViewWidth}px;
-`;
+const sidebarLayoutStyle: CSSProperties = Object.freeze({
+  position: "relative",
+  display: "grid",
+  width: "100%",
+  height: "100%",
+  gridTemplateAreas: `"${gridAreaSidebar} ${gridAreaPrimaryView} ${gridAreaSecondaryView}"`,
+});
 
 export const SidebarLayout = ({
+  className,
+  children,
   defaultSidebarWidth = 200,
   defaultSecondaryViewWidth = 200,
-  children,
 }: SidebarLayoutProps): JSX.Element => {
   const [sidebarWidth, setSidebarWidth] = useState(defaultSidebarWidth);
   const [secondaryViewWidth, setSecondaryViewWidth] = useState(
     defaultSecondaryViewWidth
   );
+  const style: CSSProperties = useMemo(
+    () => ({
+      ...sidebarLayoutStyle,
+      gridTemplateColumns: `${sidebarWidth}px 1fr ${secondaryViewWidth}px`,
+    }),
+    [sidebarWidth, secondaryViewWidth]
+  );
   return (
-    <>
-      <LayoutDiv
-        className="sidebar-layout"
-        sidebarWidth={sidebarWidth}
-        secondaryViewWidth={secondaryViewWidth}
-      >
-        <Resizer
-          position="left"
-          initialPos={sidebarWidth}
-          setWidth={setSidebarWidth}
-        />
-        <Resizer
-          position="right"
-          initialPos={secondaryViewWidth}
-          setWidth={setSecondaryViewWidth}
-        />
-        {children}
-      </LayoutDiv>
-    </>
+    <div className={className} style={style}>
+      <Resizer
+        position="left"
+        containerWidth={sidebarWidth}
+        setContainerWidth={setSidebarWidth}
+      />
+      <Resizer
+        position="right"
+        containerWidth={secondaryViewWidth}
+        setContainerWidth={setSecondaryViewWidth}
+      />
+      {children}
+    </div>
   );
 };
